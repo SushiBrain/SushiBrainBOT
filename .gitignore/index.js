2b60e385082807ad0755ce5b3b80163a2c471ebf
SushiBrain <39214346+SushiBrain@users.noreply.github.com>
2018-10-02 21:22:10 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.on("ready", function(){
-    bot.user.setGame("SushiBrain Le meilleur");
+    bot.user.setGame("SushiBrain le meilleur :heart:");
 })
 
 bot.login(process.env.TOKEN);
