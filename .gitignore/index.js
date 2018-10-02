79d714b67d27eb3167c44753d984378f94ac64ff
SushiBrain <39214346+SushiBrain@users.noreply.github.com>
2018-10-02 21:47:54 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -6,4 +6,10 @@ bot.on("ready", function(){
     bot.user.setGame("SushiBrain le meilleur");
 })
 
+clientInformation.on("message", message => {
+    if (message.content === "=Admin") {
+    message.reply("Les meilleurs Administrateurs du monde !");
+    }
+})
+
 bot.login(process.env.TOKEN);
