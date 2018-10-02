2a45b79fffffbf89d96553c1bf22caea2020663b
SushiBrain <39214346+SushiBrain@users.noreply.github.com>
2018-10-02 22:12:21 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -14,9 +14,4 @@ bot.on('message', message => {
   }
 });
 
-  // Delete a message
-message.delete()
-.then(msg => console.log(`=clear ${msg.author.username}`))
-.catch(console.error);
-
 bot.login(process.env.TOKEN);
