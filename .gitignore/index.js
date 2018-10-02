9cefe1941c7de924b7ad07fbfefdf471aaf21622
SushiBrain <39214346+SushiBrain@users.noreply.github.com>
2018-10-02 21:30:39 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -6,4 +6,36 @@ bot.on("ready", function(){
     bot.user.setGame("SushiBrain le meilleur");
 })
 
+if (message.content.startsWith('!ban')) {
+    
+    const user = message.mentions.users.first();
+    
+    if (user) {
+      
+      const member = message.guild.member(user);
+      
+      if (member) {
+        
+        member.ban({
+          reason: 'They were bad!',
+        }).then(() => {
+          
+          message.reply(`Successfully banned ${user.tag}`);
+        }).catch(err => {
+          
+          message.reply('I was unable to ban the member');
+          
+          console.error(err);
+        });
+      } else {
+        
+        message.reply('That user isn\'t in this guild!');
+      }
+    } else {
+    
+      message.reply('You didn\'t mention the user to ban!');
+    }
+  }
+});
+
 bot.login(process.env.TOKEN);
