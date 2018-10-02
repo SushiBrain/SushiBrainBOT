6a9735af3f030e4c8047dc8e24a6c967365562ba
SushiBrain <39214346+SushiBrain@users.noreply.github.com>
2018-10-02 21:57:43 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -6,4 +6,14 @@ bot.on("ready", function(){
     bot.user.setGame("SushiBrain le meilleur");
 })
 
+client.on('message', message => {
+    if (message.content === '=avatar') {
+      var member= message.mentions.members.first();
+      let embed = new Discord.RichEmbed()
+    .setImage(message.member.avatarURL)
+    .setColor('#275BF0')
+      message.channel.send(embed)
+    }
+  });
+
 bot.login(process.env.TOKEN);
