d64e3e74f288f120833d2be37cc646ca8439a03d
SushiBrain <39214346+SushiBrain@users.noreply.github.com>
2018-10-02 21:36:55 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -6,12 +6,9 @@ bot.on("ready", function(){
     bot.user.setGame("SushiBrain le meilleur");
 })
 
-client.on('message', message => {
-  
-  if (message.content === '=avatar') {
+if (message.content === '=avatar') {
     
     message.reply(message.author.avatarURL);
-  }
-});
+};
 
 bot.login(process.env.TOKEN);
