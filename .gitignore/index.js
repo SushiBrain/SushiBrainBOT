0be1ebeb0a11f2d20f57984f027268f7d1846333
SushiBrain <39214346+SushiBrain@users.noreply.github.com>
2018-10-03 21:50:15 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -15,7 +15,7 @@ bot.on('message', message => {
 });
 
 bot.on('message', message => {
-    const swearWords = ["Putain, putain"];
+    const swearWords = ["Putain", "putain","Merde","merde"];
     if( swearWords.some(word => message.content.includes(word)) ) {
         message.delete();
         message.author.send('Message supprimer pour : insulte ! Veuillez ne plus insulter sous peine de se faire ban ! Le STAFF');
