b3cccaf47b7326652809bfbd2df8287df951ff92
SushiBrain <39214346+SushiBrain@users.noreply.github.com>
2020-05-28 19:09:05 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -32,4 +32,24 @@ module.exports.run = async (bot, message, args) => {
 
 }
 
+// Create an instance of a Discord client
+const client = new Discord.Client();
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "what is my avatar"
+  if (message.content === '666avatar') {
+    // Send the user's avatar URL
+    message.reply(message.author.displayAvatarURL());
+  }
+});
+
 bot.login(process.env.TOKEN);
