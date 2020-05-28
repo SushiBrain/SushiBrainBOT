9b430c7d426139dbde68eed5b46f4d7fc4ea0436
SushiBrain <39214346+SushiBrain@users.noreply.github.com>
2020-05-28 19:23:58 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -60,23 +60,5 @@ bot.on('message', message => {
     }
 })
 
-client.on('message', async msg => {
-        if (msg.author.bot) return;
-        if (!msg.content.startsWith('!')) return
-        const args = msg.content.split(' ')
-        let command = msg.content.toLowerCase().split(' ')[0]
-        command = command.slice(PREFIX.length)
- 
-        if (command === 'play') {
-                const voiceChannel = msg.member.voiceChannel;
-                if (!voiceChannel) return msg.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
-                voiceChannel.join()
-                        .then(connection => {
-                                const dispatcher = connection.playStream(ytdl(args[1]))
-                                console.log(dispatcher.time)
-                        })
-                        .catch(console.log)
-        }
-});
 
 bot.login(process.env.TOKEN);
