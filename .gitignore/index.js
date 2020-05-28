7c17775e018f2c77c8ef655205fd68a4eafab131
SushiBrain <39214346+SushiBrain@users.noreply.github.com>
2020-05-28 19:05:07 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -32,20 +32,4 @@ module.exports.run = async (bot, message, args) => {
 
 }
 
-client.on('guildMemberAdd', member => {
-    const welcomechanel = member.guild.channels.find((x) => x.id === '713383031800594523';
-    let b_embed = new Discord.RichEmbed()
-    .setColor('RANDOM')
-    .setDescription(`Bienvenu(e) a ${member.user.username} qui nous a rejoint`)
-    return welcomechanel.send(b_embed)
-})
-
-client.on('guildMemberRemove', member => {
-    const welcomechanel = member.guild.channels.find((x) => x.id === '713383031800594523';
-    let a_embed = new Discord.RichEmbed()
-    .setColor('RANDOM')
-    .setDescription(`${member.user.username} nous a quitte`)
-    return welcomechanel.send(a_embed)
-})
-
 bot.login(process.env.TOKEN);
